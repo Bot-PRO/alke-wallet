8c8c65a71dc96821d9ad0c7d710cbbd24d49e46d
Alejandro <alejandrofreal@gmail.com>
2026-06-17 15:58:08 -0400

    initial commit

---
+++ deposit.js
@@ -0,0 +1,22 @@
+$(window).on('load', function() {
+    $("body").addClass("interfaz-lista");
+});
+
+//documento ready para que se ejecute el codigo despues de que el documento html este listo
+$(document).ready(function(){
+    evento.preventDefault();
+    
+    // del <-menu--- a los diferentes html --> deposit, sendmoney, transaction.
+
+    $("#btn-deposit").on("click", function(){
+        window.location.href = "deposit.html";
+    });
+    $("#btn-send").on("click", function(){
+        window.location.href = "sendmoney.html";
+    });
+    $("#btn-move").on("click", function(){
+        window.location.href = "transactions.html";
+    });
+
+    //Fin ---> menu
+});
